b3a6fafc1bc6dd4e10ff046f538a4dc558994e3d
kishore <kishore.asok@gmail.com>
2025-11-28 20:55:45 +0530

    Implement first version of insert test

--- tests/insert/insert.test.ts
+++ tests/insert/insert.test.ts
@@ -1,11 +1,12 @@
 import Database from 'better-sqlite3';
-import { BSON } from 'bson';
+import { BSON, ObjectId } from 'bson';
 import fs from 'fs';
 import { before, describe, it } from 'node:test';
 import { generateAndExecuteSQLFromQueryIR } from '#backend/sql-generator/index.js';
 import assert from 'assert';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import type { InsertCommandIR } from '#shared/types.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,13 +21,31 @@ before(() => {
 
 describe('insert command', () => {
   it('inserts new documents in the specified collection', () => {
-    // for (const collectionObj of collectionObjs) {
-    //   const { collection, documents } = collectionObj;
+    for (const collectionObj of collectionObjs) {
+      const { collection, documents } = collectionObj;
+      db.exec(`CREATE TABLE ${collection} (id TEXT, doc TEXT)`);
+      
+      const documentsWithId = documents.map(d => ({ ...d, _id: new ObjectId() })) as any;
 
-    //   db
-    // }
-    // generateAndExecuteSQLFromQueryIR(command)
-    assert(true);
+      const command: InsertCommandIR = {
+        command: 'insert',
+        database: 'test',
+        collection,
+        documents: documentsWithId,
+      };
+
+      generateAndExecuteSQLFromQueryIR(command, db);
+
+      const stmt = db.prepare(`SELECT doc FROM ${collection}`);
+      const stmtResult = stmt.all();
+
+      assert.equal(stmtResult.length, documents.length);
+      for (const document of documentsWithId) {
+        const resultDocJSON = (stmtResult.find((el: any) => JSON.parse(el.doc).username === document.username) as any)?.doc;
+
+        assert.equal(JSON.stringify(document), resultDocJSON);
+      }
+    }
   })
 });
 
