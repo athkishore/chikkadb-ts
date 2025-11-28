3c77e73862d6141bb377be31ebc20345c8499e73
kishore <kishore.asok@gmail.com>
2025-11-28 21:01:05 +0530

    Simplify test for insert

--- tests/insert/insert.test.ts
+++ tests/insert/insert.test.ts
@@ -4,49 +4,49 @@ import fs from 'fs';
 import { before, describe, it } from 'node:test';
 import { generateAndExecuteSQLFromQueryIR } from '#backend/sql-generator/index.js';
 import assert from 'assert';
-import { fileURLToPath } from 'url';
 import path from 'path';
 import type { InsertCommandIR } from '#shared/types.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const collection = 'users';
+const documents = [
+  {
+    _id: new ObjectId(),
+    username: 'user1',
+  },
+  {
+    _id: new ObjectId(),
+    username: 'user2',
+  },
+];
 
 let db: Database.Database;
-let collectionObjs: { collection: string; documents: Record<string, any>[] }[];
 before(() => {
   db = new Database();
-  collectionObjs = JSON.parse(fs.readFileSync(path.join(__dirname, './seed.json'), 'utf-8'));
 });
 
 describe('insert command', () => {
   it('inserts new documents in the specified collection', () => {
-    for (const collectionObj of collectionObjs) {
-      const { collection, documents } = collectionObj;
-      db.exec(`CREATE TABLE ${collection} (id TEXT, doc TEXT)`);
-      
-      const documentsWithId = documents.map(d => ({ ...d, _id: new ObjectId() })) as any;
+    db.exec(`CREATE TABLE ${collection} (id TEXT, doc TEXT)`);      
+    
+    const command: InsertCommandIR = {
+      command: 'insert',
+      database: 'test',
+      collection,
+      documents,
+    };
 
-      const command: InsertCommandIR = {
-        command: 'insert',
-        database: 'test',
-        collection,
-        documents: documentsWithId,
-      };
+    generateAndExecuteSQLFromQueryIR(command, db);
 
-      generateAndExecuteSQLFromQueryIR(command, db);
+    const stmt = db.prepare(`SELECT doc FROM ${collection}`);
+    const stmtResult = stmt.all();
 
-      const stmt = db.prepare(`SELECT doc FROM ${collection}`);
-      const stmtResult = stmt.all();
+    assert.equal(stmtResult.length, documents.length);
+    for (const document of documents) {
+      const resultDocJSON = (stmtResult.find((el: any) => JSON.parse(el.doc).username === document.username) as any)?.doc;
 
-      assert.equal(stmtResult.length, documents.length);
-      for (const document of documentsWithId) {
-        const resultDocJSON = (stmtResult.find((el: any) => JSON.parse(el.doc).username === document.username) as any)?.doc;
-
-        assert.equal(JSON.stringify(document), resultDocJSON);
-      }
+      assert.equal(JSON.stringify(document), resultDocJSON);
     }
-  })
+  });
 });
 
 
