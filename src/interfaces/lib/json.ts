4c7da41b5841cdf4035cf018aae9cfa4c0462019
Kishore Athrasseri <kishore.asok@gmail.com>
2025-12-03 11:41:53 +0530

    Define custom stringify and parse to get all tests passing

--- src/interfaces/lib/json.ts
+++ src/interfaces/lib/json.ts
@@ -2,10 +2,30 @@ import { ObjectId, EJSON } from "bson";
 
 export function stringifyToCustomJSON(value: any) {
   // return EJSON.stringify(value, { relaxed: false });
-  return JSON.stringify(value);
+  return JSON.stringify(value, (_, value) => {
+    if (value && typeof value.toHexString === 'function') {
+      return { $oid: value.toHexString() };
+    }
+
+    if (value instanceof Date) {
+      return { $date: value.toISOString() };
+    }
+
+    return value;
+  });
 }
 
 export function parseFromCustomJSON(text: string) {
   // return EJSON.parse(text, { relaxed: false });
-  return JSON.parse(text);
+  return JSON.parse(text, (_, value) => {
+    if (value && typeof value === 'object') {
+      if (value.$oid) {
+        return new ObjectId(value.$oid as string);
+      }
+      if (value.$date) {
+        return new Date(value.$date);
+      }
+    }
+    return value;
+  });
 }
