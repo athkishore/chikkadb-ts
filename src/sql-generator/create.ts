74aa95fedbca1a92a8dabac3261417e0323dd3f2
Kishore Athrasseri <kishore.asok@gmail.com>
2025-12-05 10:55:26 +0530

    Replace dot and hyphen with underscore in collection names as a temporary fix

--- src/sql-generator/create.ts
+++ src/sql-generator/create.ts
@@ -2,9 +2,15 @@ import type Database from "better-sqlite3";
 import { validateIdentifier } from "./utils.js";
 
 export function generateAndExecuteSQL_Create(command: CreateCommandIR, db: Database.Database) {
-  const isCollectionNameValid = validateIdentifier(command.collection);
+  /**
+   * This is a temporary fix for testing. The proper solution is to have a meta table
+   * mapping mongodb collection names to sqlite table names so that all valid
+   * mongodb collection names can be supported.
+   */
+  const collection = command.collection.replaceAll('.', '_').replaceAll('-', '_');
+  const isCollectionNameValid = validateIdentifier(collection);
   if (!isCollectionNameValid) throw new Error('Invalid Collection Name');
-  const statement = db.prepare(`CREATE TABLE IF NOT EXISTS ${command.collection} (id TEXT, doc TEXT)`);
+  const statement = db.prepare(`CREATE TABLE IF NOT EXISTS ${collection} (id TEXT, doc TEXT)`);
   statement.run()
   
   return { ok: 1 };
